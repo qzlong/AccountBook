cf15cc07e3dcd29c827f88ba3a25d963a751c7d0
Qzlong <861673644@qq.com>
2020-10-08 21:44:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/accountbook/KeepAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.accountbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KeepAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keep_account);
+    }
+}
