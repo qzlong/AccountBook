305ff7d5a06f53bc557982712b940b9dc719ec39
Qzlong <861673644@qq.com>
2020-10-16 18:29:51 +0800

    V0.5更改了应用框架

--- app/src/main/java/com/example/accountbook/Institute.java
+++ app/src/main/java/com/example/accountbook/Institute.java
@@ -2,6 +2,7 @@ package com.example.accountbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class Institute extends AppCompatActivity {
@@ -10,5 +11,8 @@ public class Institute extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_institute);
+        SharedPreferences.Editor editor = getSharedPreferences("Date",MODE_PRIVATE).edit();
+        //editor.
     }
+
 }
